2c8ec1af6c80d59a2ca577fc94588c10c89f331f
Wilson Neto <contato@wilsonneto.com.br>
2020-05-13 09:13:12 -0300

    Alterações para testes com videos longos

--- scripts/platforms/webos/protectedPlayer.js
+++ scripts/platforms/webos/protectedPlayer.js
@@ -9,6 +9,199 @@ webosProtectedPlayer = {
 
   appendPlayer: function (uiParentElement) {
     uiVideoElement = document.createElement('video');
+    uiVideoElement.setAttribute('controls', 'controls');
+
+    setInterval(
+      function () {
+        console.log(
+          'Ping: ReadyState => ' +
+            this.uiVideo.readyState +
+            ' - currentTime: ' +
+            this.uiVideo.currentTime
+        );
+        console.log(this.uiVideo.error);
+        console.log(this.uiVideo.readyState);
+      }.bind(this),
+      30000
+    );
+
+    uiVideoElement.addEventListener(
+      'canplay',
+      function (p) {
+        this.log('canplay');
+        this.log(p);
+      }.bind(this)
+    );
+
+    uiVideoElement.addEventListener(
+      'abort',
+      function (p) {
+        this.log('abort');
+        this.log(p);
+      }.bind(this)
+    );
+    uiVideoElement.addEventListener(
+      'canplaythrough',
+      function (p) {
+        this.log('canplaythrough');
+        this.log(p);
+      }.bind(this)
+    );
+    uiVideoElement.addEventListener(
+      'durationchange',
+      function (p) {
+        this.log('durationchange');
+        this.log(p);
+      }.bind(this)
+    );
+    uiVideoElement.addEventListener(
+      'emptied',
+      function (p) {
+        this.log('emptied');
+        this.log(p);
+      }.bind(this)
+    );
+    uiVideoElement.addEventListener(
+      'ended',
+      function (p) {
+        this.log('ended');
+        this.log(p);
+      }.bind(this)
+    );
+    uiVideoElement.addEventListener(
+      'error',
+      function (p) {
+        this.log('error');
+        this.log(p);
+      }.bind(this)
+    );
+    uiVideoElement.addEventListener(
+      'loadeddata',
+      function (p) {
+        this.log('loadeddata');
+        this.log(p);
+      }.bind(this)
+    );
+    uiVideoElement.addEventListener(
+      'loadedmetadata',
+      function (p) {
+        this.log('loadedmetadata');
+        this.log(p);
+      }.bind(this)
+    );
+
+    uiVideoElement.addEventListener(
+      'pause',
+      function (p) {
+        this.log('pause');
+        this.log(p);
+      }.bind(this)
+    );
+
+    uiVideoElement.addEventListener(
+      'loadstart',
+      function (p) {
+        this.log('loadstart');
+        this.log(p);
+      }.bind(this)
+    );
+
+    uiVideoElement.addEventListener(
+      'play',
+      function (p) {
+        this.log('play');
+        this.log(p);
+      }.bind(this)
+    );
+
+    uiVideoElement.addEventListener(
+      'playing',
+      function (p) {
+        this.log('playing');
+        this.log(p);
+      }.bind(this)
+    );
+
+    uiVideoElement.addEventListener(
+      'progress',
+      function (p) {
+        this.log('progress');
+        this.log(p);
+      }.bind(this)
+    );
+
+    uiVideoElement.addEventListener(
+      'ratechange',
+      function (p) {
+        this.log('ratechange');
+        this.log(p);
+      }.bind(this)
+    );
+
+    uiVideoElement.addEventListener(
+      'seeked',
+      function (p) {
+        this.log('seeked');
+        this.log(p);
+      }.bind(this)
+    );
+
+    uiVideoElement.addEventListener(
+      'seeking',
+      function (p) {
+        this.log('seeking');
+        this.log(p);
+      }.bind(this)
+    );
+
+    uiVideoElement.addEventListener(
+      'stalled',
+      function (p) {
+        this.log('stalled');
+        this.log(p);
+      }.bind(this)
+    );
+
+    uiVideoElement.addEventListener(
+      'seeking',
+      function (p) {
+        this.log('seeking');
+        this.log(p);
+      }.bind(this)
+    );
+
+    uiVideoElement.addEventListener(
+      'suspend',
+      function (p) {
+        this.log('suspend');
+        this.log(p);
+      }.bind(this)
+    );
+
+    uiVideoElement.addEventListener(
+      'timeupdate',
+      function (p) {
+        // this.log('timeupdate');
+        // this.log(p);
+      }.bind(this)
+    );
+
+    uiVideoElement.addEventListener(
+      'volumechange',
+      function (p) {
+        this.log('volumechange');
+        this.log(p);
+      }.bind(this)
+    );
+
+    uiVideoElement.addEventListener(
+      'waiting',
+      function (p) {
+        this.log('waiting');
+        this.log(p);
+      }.bind(this)
+    );
+
     if (uiParentElement) {
       uiParentElement.appendChild(uiVideoElement);
       this.uiVideo = uiVideoElement;
@@ -126,7 +319,7 @@ webosProtectedPlayer = {
     });
   },
 
-  registerDRMErrorHandler: function (clientId) {
+  registerDRMErrorHandler: function () {
     this.log('--registerDRMErrorHandler function:');
     var request = webOS.service.request('luna://com.webos.service.drm', {
       method: 'getRightsError',
@@ -136,12 +329,19 @@ webosProtectedPlayer = {
       },
       onSuccess: function (result) {
         this.log('registerDRMErrorHandler:success');
+        if (0 == result.errorState) {
+          this.log('*** No license');
+        } else if (1 == result.errorState) {
+          this.log('**** Invalid License');
+        }
+        console.log('DRM System ID: ' + result.drmSystemId);
+        console.log('License Server URL: ' + result.rightIssueUrl);
         this.log(result);
-      },
+      }.bind(this),
       onFailure: function (result) {
         this.log('registerDRMErrorHandler:failure');
         this.log(result);
-      },
+      }.bind(this),
     });
   },
 };
